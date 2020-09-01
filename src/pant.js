81492d1ae40637f727931495baf919fdbd7bdc61
Allison Sadin <aesadin@gmail.com>
2020-09-01 10:59:06 -0700

    add plant class
    Co-authored-by: Hannah Beinstein  <hannah.beinstein@gmail.com>

--- src/pant.js
+++ src/pant.js
@@ -1,6 +1,15 @@
-const hydrate = (plant) => {
-  return {
-    ...plant, // making a copy/inheriting behavior from plant object to instantiate new plant
-    water: (plant.water || 0) + 1
+class Plant {
+  constructor() {
+    this.water = 0;
+    this.soil = 0;
+    this.light = 0;
+  
+
+    const hydrate = (plant) => { // plant is an object being passed as a parameter 
+      return {
+        ...plant, // making a copy/inheriting behavior from plant object to instantiate new plant
+        water: (plant.water || 0) + 1 // updating the amount of water the plant receives
+      }
+    }
   }
-};
+}
